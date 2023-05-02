cf8f4ee212421b15a76290591045697369146ef2
ITP <RayhanUddinFarhad11@gmail.com>
2023-05-02 10:59:52 +0600

    getting recipes on index js

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ const express = require('express')
 const app = express()
 const port = 5000
 const chefs = require ('./chefList.json')
+const recipes = require ('./chefRecipes.json')
 
 const cors = require('cors')
 
 app.use (cors())
 
+
+
+
 app.get('/', (req, res) => {
   res.send(chefs)
+})
+
+
+
+app.get ('/recipes', (req, res) => {
+
+
+  res.send (recipes)
+ })
+
+app.get ('/:id', (req, res) => { 
+
+  const id = req.params.id;
+
+  const recipe = recipes.find ( c => c.id == id)
+
+  res.send (recipe)
+
+
+
 })
 
 app.listen(port, () => {
